a47f79a29fa11936ecfb8a7aa500b5aa7e50bd7d
GABRIEL DE OLIVEIRA TOREZAN <gabriel.torezan@a.ucb.br>
2024-03-20 11:01:06 -0300

    add objeto

---
+++ objeto/objeto.js
@@ -0,0 +1,32 @@
+let calculadora = {
+    numeroA: 0,
+    numeroB: 0,
+    somar: function(){
+        return this.numeroA + this.numeroB;
+    }
+};
+
+calculadora.numeroA = 2;
+calculadora.numeroB = 3;
+
+console.log(calculadora.somar());
+
+
+
+const pessoa = {
+    nome: "Nome da Pessoa",
+    cpf: 123456789,
+    telefone: 61999999999
+}
+
+for(const key in pessoa){
+    console.log(key + ' - ' + pessoa[key]);
+}
+
+let arr = [false, 1, "DOIS", 11, 56, pessoa];
+
+for (let i  = 0; i < arr.length; i++){
+    console.log(i + ' - ' + arr[i]);
+}
+
+let matriz = [[1, 2], [3, 4]];
