319b5f9d44b2248044d06f304b260ea4354e0c5f
GABRIEL DE OLIVEIRA TOREZAN <gabriel.torezan@a.ucb.br>
2024-03-27 09:54:39 -0300

    Adicionando atividade 3

--- atividade3/matrizTransposta.js
+++ atividade3/matrizTransposta.js
@@ -24,9 +24,9 @@ function imprimirMatriz(matriz) {
 }
 
 const matrizOriginal = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ];
 
 transporMatriz(matrizOriginal);
