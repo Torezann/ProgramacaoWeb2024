34d2d192c6cc6078a7ece89f5ab7059885af03f9
Torezann <gotorezan@gmail.com>
2024-04-09 17:16:08 -0300

    Adicionando modulos

--- modulos/index.js
+++ modulos/index.js
@@ -3,11 +3,11 @@ const calc = require('./util/calculadora');
 const express = require('express');
 const app = express();
 
-app.get('/ola', function(req, res){
+app.get('/ola', function (req, res) {
     res.send('Olá, mundo!');
 });
 
 const PORT = 8080;
-app.listen(PORT, function(){
-    console.log('app rodando na porta '+ PORT);
+app.listen(PORT, function () {
+    console.log('app rodando na porta ' + PORT);
 });
