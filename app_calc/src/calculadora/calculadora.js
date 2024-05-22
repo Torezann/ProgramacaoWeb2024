90d5ec6d8c759cdb1423ace59d9f382f6ea74e73
GABRIEL DE OLIVEIRA TOREZAN <gabriel.torezan@a.ucb.br>
2024-05-22 10:59:38 -0300

    Adicionando calculadora

---
+++ app_calc/src/calculadora/calculadora.js
@@ -0,0 +1,55 @@
+const { resultado } = require("../controller/calculadoraController");
+
+function calcular(v1, v2, op){
+    switch (op) {
+        case '+':
+            resultado = v1 + v2;
+            
+            break;
+        case '-':
+            resultado = v1 - v2;
+
+            break;
+        case '*':
+            resultado = v1 * v2;
+            
+            break;
+        case '/':
+            resultado = v1 / v2;
+            
+            break;
+        default:
+            break;
+    }
+    return resultado;
+}
+
+function nomeOperador(op){
+    let nmOperador;
+    switch (op) {
+        case '+':
+            nmOperador = "soma";
+            
+            break;
+        case '-':
+            nmOperador = "subtração";
+
+            break;
+        case '*':
+            nmOperador = "multiplcação";
+            
+            break;
+        case '/':
+            nmOperador = "divisão";
+            
+            break;
+        default:
+            break;
+    }
+    return nmOperador;
+}
+
+module.exports = {
+    calcular,
+    nomeOperador
+}
